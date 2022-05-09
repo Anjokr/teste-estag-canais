7d376d84a56a89b85467d83aec5886ee4632e5c7
Anjokr <solon7833@gmail.com>
2022-05-09 15:46:36 -0300

    edit variables type

--- my_program/Operation.java
+++ my_program/Operation.java
@@ -1,16 +1,14 @@
 package my_program;
 
 public class Operation {
-    Person sender;
-    Person receiver;
-    int transferId;
-    double transferValue;
-    String transferType;
+    private Person sender;
+    private Person receiver;
+    private double transferValue;
+    private String transferType;
 
-    public Operation(Person sender, Person receiver, int id, double value, String type) {
+    public Operation(Person sender, Person receiver, double value, String type) {
         this.sender = sender;
         this.receiver = receiver;
-        transferId = id;
         transferValue = value;
         transferType = type;
     }
