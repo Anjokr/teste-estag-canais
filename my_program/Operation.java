6f50f47742c9699516ee41c7618575b9001100bc
Anjokr <solon7833@gmail.com>
2022-05-09 15:35:03 -0300

    operation file completed

--- my_program/Operation.java
+++ my_program/Operation.java
@@ -14,4 +14,42 @@ public class Operation {
         transferValue = value;
         transferType = type;
     }
+
+    public void start() {
+        //check if the value match the transfer type
+        if(checkTypeValue()) return;
+        sender.subtractBalance(transferValue);
+        receiver.addBalance(transferValue);
+
+        System.out.println("Sua transferência foi realizada com sucesso!");
+        System.out.println("Saldo do emissor: R$ " + String.format("%.2f", sender.getBalance()));
+        System.out.println("Saldo do receptor: R$ " + String.format("%.2f", receiver.getBalance()));
+    }
+
+    public boolean checkTypeValue() {
+        if(transferType.equals("PIX")) {
+            if(transferValue > 5000) {
+                //error message
+                System.out.println("Sua transferência de valor: "+ transferValue+" não foi completada por ser valor não permitida através do PIX. Valor deve ser abaixo de R$ 5 mil");
+                return true;
+            }
+        }
+        if(transferType.equals("TED")) {
+            if(transferValue < 5000 || transferValue > 10000) {
+                //error message
+                System.out.println("Sua transferência de valor: "+ transferValue+" não foi completada por ser valor não permitida através do TED. Valor deve ser entre R$ 5 mil e R$ 10 mil");
+                return true;
+            }
+        }
+        if(transferType.equals("DOC")) {
+            if(transferValue < 10000) {
+                //error message
+                System.out.println("Sua transferência de valor: "+ transferValue+" não foi completada por ser valor não permitida através do DOC. Valor deve ser acima de R$ 10 mil");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
